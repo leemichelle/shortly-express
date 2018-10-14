0c3c04960a35f2372c4e3c829bda810a77fa890c
leemichelle <mlee749@gmail.com>
2018-10-13 17:01:22 -0700

    passes cookie test, added comments for auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,14 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  // req.cookies;
+  // req.session;
+  req.session = req.cookies;
+  console.log(req);
+  // models.compare(attempt, passw, salt)
+  
+  // console.log('response', res);
+  // console.log('session', req.session);
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 const parseCookies = (req, res, next) => { 
   let cookie = req.headers.cookie;
-  let cookieSplit = {};
+  let cookiesParsed = {};
   if (cookie) {
     let cookieArr = cookie.split(';');
     for (let i = 0; i < cookieArr.length; i++) {
       let secondCookieArr = cookieArr[i].split('=');
-      cookieSplit[secondCookieArr[0].trim()] = secondCookieArr[1].trim();
+      cookiesParsed[secondCookieArr[0].trim()] = secondCookieArr[1].trim();
     }
   }
-  req.cookies = cookieSplit;
+  req.cookies = cookiesParsed;
   next();
 };
 
